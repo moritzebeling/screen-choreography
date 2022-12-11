22e6197d4d87406357d6986c9c93bed88e7b8262
moritzebeling <git@moritzebeling.com>
2022-12-11 18:49:08 +0100

    time ticker and index page

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script>
+    
+    import { tick } from "$lib/system";
+    import { onMount } from "svelte";
+
+    onMount(()=>{
+        tick();
+    });
+    
+</script>
+
+<slot />
---
+++ src/routes/+page.svelte
@@ -0,0 +1,61 @@
+<script>
+  
+    import { duration, seconds, time } from "$lib/system";
+
+    let choreographies = [
+    ];
+    
+</script>
+
+<header>
+    <h1>Client Choreography</h1>
+    <nav>
+        {#each choreographies as link}
+            <a href="/{link}">{link}</a>
+        {/each}
+    </nav>
+</header>
+
+<main>
+    <p>{$seconds}</p>
+</main>
+
+<aside>
+    <p>{$time}</p>
+</aside>
+
+<style>
+
+    header {
+        padding: 1rem;
+        position: fixed;
+        top: 0;
+        left: 0;
+    }
+
+    nav {
+        display: flex;
+        gap: 0.5em;
+    }
+
+    main {
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        text-align: center;
+        font-size: 10vw;
+    }
+
+    aside {
+        padding: 1rem;
+        position: fixed;
+        bottom: 0;
+        left: 0;
+    }
+
+</style>
