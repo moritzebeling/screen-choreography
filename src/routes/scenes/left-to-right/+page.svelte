57e0914bbed26c8fee1fe833f3d004788aac3fc7
moritzebeling <git@moritzebeling.com>
2022-12-12 18:15:13 +0100

    add qr scene

---
+++ src/routes/scenes/left-to-right/+page.svelte
@@ -0,0 +1,53 @@
+<script>
+
+    import { seconds } from "$lib/system";
+
+    let animating = false;
+    let alternated = false;
+
+    function switchTarget( s ){
+        if( s % 5 === 0 ){
+            animating = true;
+        }
+    }
+
+    $: switchTarget( $seconds );
+
+    /*
+    @todo ever seconds screen needs to be alternated
+    */
+
+</script>
+
+<svelte:window on:click={() => alternated = true } />
+
+<main class:animating class:alternated></main>
+
+<style>
+
+    main {
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100vw;
+        height: 100%;
+    }
+    main.animating {
+        animation: slide 2s linear;
+        animation-iteration-count: infinite;
+        background: linear-gradient(90deg, rgba(0,0,0,1) 0%, rgba(255,255,255,1) 100%);
+    }
+    main.animating.alternated {
+        animation-delay: 1s;
+    }
+
+    @keyframes slide {
+        from {
+            transform: translateX(-100vw);
+        }
+        to {
+            transform: translateX(100vw);
+        }
+    }
+
+</style>
---
+++ src/routes/scenes/qr/+page.svelte
@@ -0,0 +1,59 @@
+<script>
+
+    import { seconds } from "$lib/system";
+
+    let animating = false;
+
+    function switchTarget( s ){
+        if( s % 3 === 1 ){
+            animating = true;
+        }
+    }
+
+    $: switchTarget( $seconds );
+
+    /* @todo insert qe code */
+
+</script>
+
+<main>
+    <figure class:animating><h1>QR</h1></figure>
+</main>
+
+<style>
+
+    main {
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100vw;
+        height: var(--100vh);
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        background-color: white;
+    }
+
+    figure {
+        width: 70vh;
+        height: 70vh;
+        background-color: black;
+        color: white;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+    figure.animating {
+        animation: hovering 5s ease-in-out infinite;
+    }
+
+    @keyframes hovering {
+        0%, 100% {
+            transform: scale(1);
+        }
+        50% {
+            transform: scale(0.5);
+        }
+    }
+
+</style>
