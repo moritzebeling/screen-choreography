58e9ccb1b1c579d82504dc06c685b52109d5fad0
moritzebeling <git@moritzebeling.com>
2022-12-12 13:00:02 +0100

    add single control button

---
+++ src/routes/control/color/+page.svelte
@@ -0,0 +1,37 @@
+<script>
+    
+    import ControlButton from "$lib/ControlButton.svelte";
+
+</script>
+
+<nav>
+    <ControlButton id={3}>Update</ControlButton>
+</nav>
+
+<style>
+
+    nav {
+        padding: 1rem;
+        width: 100vw;
+        height: 100vh;
+        display: flex;
+        position: fixed;
+        left: 0;
+        top: 0;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+        font-size: 6rem;
+    }
+
+    nav :global( button ){
+        margin: 0.3rem;
+    }
+    
+    @media (orientation: portrait){
+        nav :global( button ){
+            transform: rotate(-90deg);
+        }
+    }
+
+</style>
