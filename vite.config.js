485f805a5fdc2230efa320c15488844ed843a4a4
moritzebeling <git@moritzebeling.com>
2022-12-12 18:59:17 +0100

    install socket.io

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,34 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import { Server } from 'socket.io';
 
 /** @type {import('vite').UserConfig} */
 const config = {
-	plugins: [sveltekit()]
+	plugins: [
+		sveltekit(),
+		{
+			name: 'sveltekit-socket-io',
+			configureServer(server) {
+
+				const io = new Server(server.httpServer);
+				// Socket.IO stuff goes here
+				console.log('SocketIO injected');
+
+				// This is located in the svelte config (see above "Socket.IO stuff goes here")
+				io.on('connection', (socket) => {
+					// Generate a random username and send it to the client to display it
+					let username = `User ${Math.round(Math.random() * 999999)}`;
+					socket.emit('name', username);
+
+					// Receive incoming messages and broadcast them
+					socket.on('setScene', (scene) => {
+						io.emit('setScene', scene);
+					});
+				});
+
+
+			}
+		}
+	]
 };
 
 export default config;
