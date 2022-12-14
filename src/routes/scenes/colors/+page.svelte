e8135d396d484cf69927cd661c5ec9708c295ddf
moritzebeling <git@moritzebeling.com>
2022-12-14 09:46:57 +0100

    simplify color scene

--- src/routes/scenes/colors/+page.svelte
+++ src/routes/scenes/colors/+page.svelte
@@ -16,44 +16,8 @@
     let s = 0;
     let i = 0;
     let speed = 5000;
-    let inAutopilot = false;
-    let speedUp = 0;
-
-    function autoPilot(){
-
-        let p = (i - $user.num) % $users.ordered;
-
-        color = p === 0 ? white : black;
-
-        console.log( i, speed, color );
-
-        if( speedUp >= 0 ){
-            speed = morph( speed, 150, 50, 0.05 );
-            if( speed <= 150 ){
-                speedUp++;
-            }
-            if( speedUp > 80 ){
-                speedUp = -1;
-            }
-        } else {
-            speed = morph( speed, 5000, 50 );
-        }
-
-        i++;
-        setTimeout(() => {
-            autoPilot();
-        }, speed);
-    }
 
     function switchColor(){
-
-        if( speed < 2000 || inAutopilot ){
-            if( !inAutopilot ){
-                inAutopilot = true;
-                autoPilot();
-            }
-            return;
-        }
 
         s++;
         if( s * 1000 < speed ){
@@ -61,18 +25,14 @@
         }
         s = 0;
 
-        speed = morph( speed, 0, 50, 0.075 );
-
         if( i < 1 ){
             color = blue;
-        // } else if( speed > 2500 ) {
+        } else if( i > 3 ) {
+            color = black;
         } else {
             color = [red,blue,green][ (i + 100 - $user.num) % 3 ];
-        // } else {
-        //     color = [white,black][ ($user.num+i) % 2 ];
         }
 
-        console.log( i, speed, color );
         i++;
     }
 
