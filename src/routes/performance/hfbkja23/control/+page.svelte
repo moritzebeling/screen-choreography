c9fdb471a2132b13a921fab174e9e5f731460fa0
moritzebeling <git@moritzebeling.com>
2023-02-08 14:54:59 +0100

    throttle autosend controls

--- src/routes/performance/hfbkja23/control/+page.svelte
+++ src/routes/performance/hfbkja23/control/+page.svelte
@@ -1,15 +1,6 @@
 <script>
 
-    /*
-    scene options
-    - background color
-        - all the same or offset by user num
-    - stay on or fade out
-        - fade out speed
-    - rotate or synchronize
-        - rotation speed
-    */
-
+    import throttle from 'just-throttle';
     import { socket } from "../socket.js";
     import { performanceStore } from "$lib/stores";
     import { PerformanceScene } from "$lib/models/PerformanceScene.js";
@@ -19,20 +10,19 @@
 
     let autoSend = false;
     let pendingChanges = false;
-
-    function send(){
+    
+    const send = throttle(() => {
         pendingChanges = false;
         console.log('scene:update', scene);
         socket.emit('scene:update', scene);
         performanceStore.set(scene);
-    }
+    }, 500, {trailing: true});
+
     function update(_){
-        if( !autoSend ){
-            pendingChanges = true;
-            return;
+        pendingChanges = true;
+        if( autoSend ){
+            send();
         }
-        // todo throttle
-        send();
     }
     $: update( scene );
 
@@ -61,7 +51,7 @@
             <p>Background speed</p>
             <p>{scene.speed}ms</p>
         </div>
-        <input type="range" min="0" max="5000" bind:value={scene.speed} />
+        <input type="range" min="0" step="10" max="5000" bind:value={scene.speed} />
     </label>
 
     <label class="col-3">
@@ -69,7 +59,7 @@
             <p>Fade In</p>
             <p>{scene.fadeIn}ms</p>
         </div>
-        <input type="range" min="0" max="5000" bind:value={scene.fadeIn} />
+        <input type="range" min="0" step="10" max="5000" bind:value={scene.fadeIn} />
     </label>
 
     <label class="col-3">
@@ -77,14 +67,14 @@
             <p>Fade Out</p>
             <p>{scene.fadeOut}ms</p>
         </div>
-        <input type="range" min="0" max="5000" bind:value={scene.fadeOut} />
+        <input type="range" min="0" step="10" max="5000" bind:value={scene.fadeOut} />
     </label>
 
     <button class="col-3" class:alert={autoSend} on:click={()=> autoSend = !autoSend }>
         Auto push
     </button>
     
-    <button class="col-1" on:click={()=> scene.interval = Math.max(30,Math.round(scene.interval * 0.8)) }>
+    <button class="col-1" on:click={()=> scene.interval = Math.max(50,Math.round(scene.interval * 0.8)) }>
         -20%
     </button>
     
@@ -93,7 +83,7 @@
             <p>Interval speed</p>
             <p>{scene.interval}ms</p>
         </div>
-        <input type="range" min="100" max="5000" bind:value={scene.interval} />
+        <input type="range" min="50" step="10" max="5000" bind:value={scene.interval} />
     </label>
 
     <button class="col-1" on:click={()=> scene.interval = Math.round(scene.interval * 1.2) }>
